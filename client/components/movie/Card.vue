f647db5b80e566f03cf61d8033e467f4bf0aade9
Lazar <lazazr@gmail.com>
2020-05-16 20:51:33 +0200

    feat: add movie title on hover with card layout

--- client/components/movie/Card.vue
+++ client/components/movie/Card.vue
@@ -2,11 +2,15 @@
   <div
     class="relative duration-500 ease-in-out cursor-pointer hover:shadow-md md:transform md:transition hover:-translate-y-1 hover:scale-105"
   >
+    <div class="absolute z-20 w-full h-full px-3 py-2 duration-500 bg-opacity-75 rounded-md opacity-0 bg-m-blue-900 hover:opacity-100">
+      <p class="font-bold leading-5 text-gray-300 "
+         :class="movieInfos.title.length < 30 ? 'text-lg' : 'text-base'"
+      >{{ movieInfos.title.length > 100 ? movieInfos.title.slice(0,100) + '...' : movieInfos.title }}</p>
+    </div>
     <img
       class="object-cover rounded-md md:w-card-lg md:h-card-lg w-card h-card"
       :src="movieInfos.poster_path !== null ? imgPath + movieInfos.poster_path : require('~/assets/img/noPoster.jpg')"
       :alt="'Poster of ' + movieInfos.title "
-      :title="movieInfos.title"
     />
     <span
       :class="movieInfos.vote_average > 5.1 ? 'bg-green-500' : 'bg-red-500'"

403bfc5bc64dcfa18650eb889e4a901335f8f919
Lazar <lazazr@gmail.com>
2020-05-23 19:19:36 +0200

    refactor: add small validation for quick search

--- client/components/global/QuickSearch.vue
+++ client/components/global/QuickSearch.vue
@@ -1,31 +1,42 @@
 <template>
   <div class="w-full lg:flex lg:justify-end">
-    <form @submit.prevent="prepareSearch">
-      <div class="relative">
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg class="w-5 h-5 text-gray-300" fill="currentColor" viewBox="0 0 20 20">
-            <path
-              fill-rule="evenodd"
-              d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-              clip-rule="evenodd"
+    <ValidationObserver ref="form" v-slot="{ handleSubmit }">
+      <form @submit.prevent="handleSubmit(prepareSearch)">
+        <div class="relative">
+          <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg class="w-5 h-5 text-gray-300" fill="currentColor" viewBox="0 0 20 20">
+              <path
+                fill-rule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </div>
+          <ValidationProvider v-slot="{ failed, changed }" mode="passive" rules="required">
+            <input
+              v-model="query.movieName"
+              id="search"
+              :class=" failed && !changed ? 'border-red-400' : 'border-transparent'"
+              class="block w-full py-2 pl-10 pr-3 leading-5 text-white placeholder-gray-300 border border-transparent rounded-md lg:transition-all bg-m-blue-900 lg:bg-transparent focus:outline-none hover:bg-m-blue-900 focus:bg-m-blue-900 sm:text-sm lg:duration-300 lg:ease-in lg:w-40 lg:focus:w-96"
+              placeholder="Quick search"
+              type="search"
             />
-          </svg>
+          </ValidationProvider>
         </div>
-        <input
-          v-model="query.movieName"
-          id="search"
-          class="block w-full py-2 pl-10 pr-3 leading-5 text-white placeholder-gray-300 border border-transparent rounded-md lg:transition-all bg-m-blue-900 lg:bg-transparent focus:outline-none hover:bg-m-blue-900 focus:bg-m-blue-900 sm:text-sm lg:duration-300 lg:ease-in lg:w-40 lg:focus:w-96"
-          placeholder="Quick search"
-          type="search"
-        />
-      </div>
-    </form>
+      </form>
+    </ValidationObserver>
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
+import { ValidationProvider, ValidationObserver } from 'vee-validate'
+
 export default {
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
   data () {
     return {
       query: {

4acb7b2be67099128f1caa0c5285526c9ed0576f
Lazar <lazazr@gmail.com>
2020-06-02 00:19:21 +0200

    refactor: smaller controller and fix to dev in local in axios plugin

--- client/plugins/axios.js
+++ client/plugins/axios.js
@@ -1,9 +1,19 @@
 export default function ({ app, $axios }) {
-  if (process.client) {
-    $axios.setBaseURL(app.$env.API_BROWSER_URL)
-  }
+  if (process.env.NODE_ENV === 'production') {
+    if (process.client) {
+      $axios.setBaseURL(app.$env.API_BROWSER_URL)
+    }
 
-  if (process.server) {
-    $axios.setBaseURL(app.$env.API_URL)
+    if (process.server) {
+      $axios.setBaseURL(app.$env.API_URL)
+    }
+  } else {
+    if (process.client) {
+      $axios.setBaseURL(process.env.API_BROWSER_URL)
+    }
+
+    if (process.server) {
+      $axios.setBaseURL(process.env.API_URL)
+    }
   }
 }

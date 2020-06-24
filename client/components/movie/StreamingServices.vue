a63fc5b0e51150e628b255ae9c63d6739c48f3d2
Lazar <lazazr@gmail.com>
2020-06-24 21:29:05 +0200

    feat: create component for streaming services in movie page

---
+++ client/components/movie/StreamingServices.vue
@@ -0,0 +1,31 @@
+<template>
+  <section id="services" class="relative w-full mt-6 sm:mx-0">
+    <div class="w-11/12 mx-auto sm:w-full">
+      <h3 class="mb-1 text-2xl font-extrabold text-gray-300 md:ml-3 sm:mb-0 sm:text-2xl">Watch On</h3>
+    </div>
+    <div class="flex flex-wrap items-center flex-shrink-0 mx-6 space-x-4">
+      <span v-for="(service, index) in infos.collection.locations" :key="index">
+        <a :href="service.url" target="_blank" rel="noopener">
+          <img :src="service.icon" alt="service.display_name" class="">
+        </a>
+      </span>
+    </div>
+
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    infos: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
